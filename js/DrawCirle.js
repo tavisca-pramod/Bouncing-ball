0357aabce7a1ca0298c731d186e0f5afab3883bf
pramod <pramod@tavisca.com>
2014-07-27 19:27:30 +0530

    Refactored to move circle on x and y axis by 5 points

--- js/DrawCirle.js
+++ js/DrawCirle.js
@@ -2,15 +2,30 @@
 //initialize the diagram
 //fill color in circle
 //draw circle
+//draw after an interval
+//move circle position
 
 var boouncingBallContext;
+
+var postionOnXAxis=50;
+var postionOnYAxis=270;
+var radius = 20;
+var startAngle =0;
+var endAngle = Math.PI*2;//THTS A FULL CIRCLE
+var clockDirection = true;
+var pointsToMoveOnXAxis=5;
+var pointsToMoveOnYAxis=5;
+var canvasHeight = 500;
+var canvasWidth = 500;
 
 
 function setContext()
 {   
-    boouncingBallContext = bouncingBallCanvas.getContext('2d');    
+    boouncingBallContext = bouncingBallCanvas.getContext('2d');
 
-    boouncingBallContext.beginPath();
+    setInterval(drawCirle,100);
+
+    /*boouncingBallContext.beginPath();
 
     boouncingBallContext.fillStyle = "#070719";
 
@@ -18,5 +33,20 @@ function setContext()
 
     boouncingBallContext.closePath();
 
-    boouncingBallContext.fill();
+    boouncingBallContext.fill();*/
 }
+
+function drawCirle()
+{
+  boouncingBallContext.clearRect(0,0, canvasWidth,canvasHeight);
+  boouncingBallContext.beginPath();
+  boouncingBallContext.fillStyle="#070719";
+  
+  boouncingBallContext.arc(postionOnXAxis,postionOnYAxis,radius,startAngle,endAngle,clockDirection);
+  boouncingBallContext.closePath();
+  boouncingBallContext.fill();
+   
+  postionOnXAxis+=pointsToMoveOnXAxis; 
+  postionOnYAxis+=pointsToMoveOnYAxis; 
+}
+
